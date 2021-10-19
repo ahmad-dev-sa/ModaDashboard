fda21ff62d2de3eb3df78c08042a8f5364131098
ahmad alonazi <59384646+ahmad-dev-sa@users.noreply.github.com>
2021-10-19 11:52:17 +0300

    UPDATE

--- js/main.js
+++ js/main.js
@@ -49,12 +49,16 @@ $(document).ready(function(){
     });
 
 
-  
+    $('#framework').multiselect({
+        nonSelectedText: 'Select Framework',
+        enableFiltering: true,
+        enableCaseInsensitiveFiltering: true,
+        buttonWidth:'400px'
+       });
 
  });
 
  
-
 
 
 
